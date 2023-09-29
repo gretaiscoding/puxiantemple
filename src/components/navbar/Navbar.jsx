8d751f8c79d6930b0f6aec4dfd6121b8c16bad8a
taukelim <109900562+taukelim@users.noreply.github.com>
2023-09-29 20:10:47 +0800

    Add navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+const Navbar = () => {
+  return (
+    <div>
+      <h2>PuXian Temple</h2>
+      <div className="links">
+        <a href="">About Us</a>
+        <a href="">Contact Us</a>
+        <a href="">Learn Buddhism</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
