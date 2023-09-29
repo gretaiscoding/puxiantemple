66c971e841b452b6686b95ddcef684d6325cd92f
taukelim <109900562+taukelim@users.noreply.github.com>
2023-09-29 20:18:09 +0800

    Added details to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <h2>PuXian Temple</h2>
+      <div className="nav-links">
+        <a href="">About Us</a>
+        <a href="">Contact Us</a>
+        <a href="">Learn Buddhism</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
