07a376007556574ae46a9e0daf994a336057b622
taukelim <109900562+taukelim@users.noreply.github.com>
2023-10-01 23:41:50 +0800

    Added Hero to app, and made app a flex box with direction column

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Navbar />
-    </>
+      <Hero />
+    </div>
   );
 }
 
