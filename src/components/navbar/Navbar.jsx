4e53243530655d8712ee7bf493402621544d9a69
taukelim <109900562+taukelim@users.noreply.github.com>
2023-11-07 11:36:14 +0800

    Merge latest Navbar code to master

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import './navbar.css';
-<<<<<<< HEAD
-const Navbar = () => {
-  return (
-    <div>
-      <h2>PuXian Temple</h2>
-      <div className="links">
-        <a href="">About Us</a>
-        <a href="">Contact Us</a>
-        <a href="">Learn Buddhism</a>
-      </div>
-    </div>
-=======
 
 const Navbar = () => {
   return (
     <nav>
-      <h2 id='logo-name'>PuXian Temple</h2>
+      <h2 id="logo-name">PuXian Temple</h2>
       <div className="nav-links">
         <a href="">About</a>
         <a href="">Events</a>
         <a href="">Contact Us</a>
-        <a href="" id='donate-btn'>Donate</a>
+        <a href="" id="donate-btn">
+          Donate
+        </a>
       </div>
     </nav>
->>>>>>> 48be2e099304d044929da6c9581d15f4d10b23df
   );
 };
 
