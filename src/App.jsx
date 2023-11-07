79ffbdc296c932bf90b607924bad9a377853f829
taukelim <109900562+taukelim@users.noreply.github.com>
2023-11-07 13:06:26 +0800

    Added About component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <About />
     </div>
   );
 }
