865409dd0bbd59cc331e844f8b4912a601260cde
taukelim <109900562+taukelim@users.noreply.github.com>
2023-11-07 11:59:05 +0800

    Added Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,14 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <nav>
-      <h2>PuXian Temple</h2>
+      <h2>Pu Xian Temple</h2>
       <div className="nav-links">
         <a href="">About</a>
         <a href="">Events</a>
         <a href="">Contact Us</a>
-        <a href="" id='donate-btn'>Donate</a>
+        <a href="" id="donate-btn">
+          Donate
+        </a>
       </div>
     </nav>
   );
