986315611bccea34d7ac23ce642ee06918d445cd
taukelim <109900562+taukelim@users.noreply.github.com>
2023-12-11 20:59:46 +0800

    Responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
 
 const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  useEffect(() => {
+    if (isNavExpanded) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isNavExpanded]);
+
   return (
     <nav>
       <h2 className="nav__logo">Pu Xian Temple</h2>
-      <div className="nav__links">
-        <a href="">About</a>
-        <a href="">Events</a>
-        <a href="">Contact Us</a>
-        <a href="" id="donate-btn">
+      <button
+        className="nav__hamburger"
+        onClick={() => setIsNavExpanded(!isNavExpanded)}
+      >
+        Menu
+      </button>
+      <div className={`nav__links ${isNavExpanded ? 'expanded' : ''}`}>
+        <button className="nav__close" onClick={() => setIsNavExpanded(false)}>
+          Close
+        </button>
+        <a href="#">About</a>
+        <a href="#">Events</a>
+        <a href="#">Contact Us</a>
+        <a href="#" id="donate-btn">
           Donate
         </a>
       </div>
